64126b45fa29e4b24f8d921a8fc4f9bc5f1dbfbd
santiago <santi2502zaron@gmail.com>
2026-09-07 06:03:21 -0500

    fix: actualizar firebase para usar variables secretas

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZKaEPbw3D-FmKLxiWLHiYfXsyw6fmW8s",
-  authDomain: "rossely-8af03.firebaseapp.com",
-  projectId: "rossely-8af03",
-  storageBucket: "rossely-8af03.firebasestorage.app",
-  messagingSenderId: "1022004792497",
-  appId: "1:1022004792497:web:9f872b282364e0ba4ea690",
-  measurementId: "G-1QRW4QZ5CT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBZKaEPbw3D-FmKLxiWLHiYfXsyw6fmW8s",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "rossely-8af03.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "rossely-8af03",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "rossely-8af03.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1022004792497",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:1022004792497:web:9f872b282364e0ba4ea690"
 };
 
 const app = initializeApp(firebaseConfig);
