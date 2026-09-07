4945b71fa580bb13bad7540e8023ef70e2326ffe
santiago <santi2502zaron@gmail.com>
2026-09-07 05:46:36 -0500

    fix: corregir api key exacta de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZKaEPbW3D-FmKLxiWLHiYfXsyw6fmW8s",
+  apiKey: "AIzaSyBZKaEPbw3D-FmKLxiWLHiYfXsyw6fmW8s",
   authDomain: "rossely-8af03.firebaseapp.com",
   projectId: "rossely-8af03",
   storageBucket: "rossely-8af03.firebasestorage.app",
   messagingSenderId: "1022004792497",
   appId: "1:1022004792497:web:9f872b282364e0ba4ea690",
-  measurementId: "G-1QKW4QZ5CT"
+  measurementId: "G-1QRW4QZ5CT"
 };
 
 const app = initializeApp(firebaseConfig);
