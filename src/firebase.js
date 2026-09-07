2d7fed1ff2a08a246056ed312e05592d530334e4
santiago <santi2502zaron@gmail.com>
2026-09-07 03:45:33 -0500

    fix: configurar credenciales reales de firebase rossely

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzKaEPhxBD-FmKLx1MLItfYxcy6WFk6E",
+  apiKey: "AIzaSyBZKaEPbw3D-FmKLxiWlHYiFxsyW6fmW8s",
   authDomain: "rossely-8af03.firebaseapp.com",
   projectId: "rossely-8af03",
   storageBucket: "rossely-8af03.firebasestorage.app",
   messagingSenderId: "1022004792497",
-  appId: "1:1022004792497:web:9f872b282364e8ba4ee498",
-  measurementId: "G-1QKW4QZ5CT"
+  appId: "1:1022004792497:web:9f872b282364e0ba4ea690",
+  measurementId: "G-1QRW4QZ5CT"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exportar servicios de base de datos y autenticación
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
